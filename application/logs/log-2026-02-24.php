149c2ed1f6bcd220ab39b68b2830a1e4aa266061
NinsonD <godclutch958@gmail.com>
2026-03-04 18:05:41 +0400

    Initial commit: Add hit-ace website project

---
+++ application/logs/log-2026-02-24.php
@@ -0,0 +1,50 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-02-24 04:23:23 --> Severity: error --> Exception: Unknown database 'acon' C:\xampp\htdocs\hit-ace\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2026-02-24 13:24:40 --> Severity: error --> Exception: Table 'hitace.tbl_settings' doesn't exist C:\xampp\htdocs\hit-ace\system\database\drivers\mysqli\mysqli_driver.php 305
+ERROR - 2026-02-24 13:24:40 --> Severity: error --> Exception: Table 'hitace.tbl_settings' doesn't exist C:\xampp\htdocs\hit-ace\system\database\drivers\mysqli\mysqli_driver.php 305
+ERROR - 2026-02-24 13:25:26 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:25:50 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:27:37 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:27:37 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:28:26 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:28:26 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:34:20 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:34:35 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:34:35 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:34:36 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:34:37 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:34:37 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:35:18 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:35:19 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:02 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:26 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:32 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:47 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:48 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:52 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:36:56 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 13:37:04 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 17:41:16 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 17:41:29 --> Severity: 8192 --> setcookie(): Passing null to parameter #2 ($value) of type string is deprecated C:\xampp\htdocs\hit-ace\system\libraries\Session\Session_driver.php 142
+ERROR - 2026-02-24 17:42:13 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 17:42:50 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 17:43:09 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 17:43:36 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:07:47 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:07:50 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:08:28 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:13:39 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:29:30 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:37:13 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:37:36 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:37:39 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:37:46 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:37:56 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:37:57 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:38:01 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:38:22 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:38:28 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:38:52 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:38:57 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-02-24 18:40:59 --> 404 Page Not Found: Public/style.css
---
+++ application/logs/log-2026-03-02.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-03-02 12:36:06 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 12:37:42 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 12:37:44 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 12:37:52 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 12:37:52 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 12:37:53 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 18:52:20 --> 404 Page Not Found: Public/style.css
+ERROR - 2026-03-02 18:52:22 --> 404 Page Not Found: Public/style.css
